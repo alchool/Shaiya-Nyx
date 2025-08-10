ee865d243bf0ee148e226a258a2d4d43b9ad586d
alchool <64260154+alchool@users.noreply.github.com>
2025-08-10 23:01:12 +0200

    Delete deps.py

--- deps.py
+++
@@ -1,7 +0,0 @@
-from fastapi import Request, HTTPException, status
-
-def get_current_user(request: Request):
-    user = request.session.get("user")
-    if not user:
-        raise HTTPException(status_code=status.HTTP_302_FOUND, headers={"Location": "/login"})
-    return user
